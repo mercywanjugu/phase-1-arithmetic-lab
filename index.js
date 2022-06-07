a07267045664004b423568cb1763a0fcf8693ee2
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-07 14:18:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 1;
+const num2 = 62;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 24;
+const num4 = 10;
+
+const mod = num3 % num4;
+
+const max = Math.max(3, 7, 11, 16, 19, 20);
